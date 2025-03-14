cd077eacef4567756595b1962d5e03afdcf79976
Artur Anisimov <anisimov.arthur@gmail.com>
2025-03-14 12:38:52 +0100

    SDKUpdater

---
+++ Code/Updater/SDKUpdater.cs
@@ -0,0 +1,86 @@
+#if UNITY_EDITOR
+using System.IO;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Test
+{
+    public class SDKUpdater : EditorWindow
+    {
+        [MenuItem("Tools/SDK Updater")]
+        public static void Open()
+        {
+            var window = EditorWindow.GetWindow<SDKUpdater>();
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            SDKUpdateGUI();
+            GUILayout.Space(10);
+        }
+
+        private void SDKUpdateGUI()
+        {
+            GUILayout.Label("SDK Update");
+
+            if (GUILayout.Button("Create assemblies"))
+            {
+                CreateAssemblyDefinition("ROAS/Scripts", "ROAS.Runtime", "MaxSdk.Scripts");
+                CreateAssemblyDefinition("Tenjin/Scripts", "Tenjin.Runtime", "MaxSdk.Scripts");
+                CreateAssemblyDefinition("Tenjin/Scripts/Editor", "Tenjin.Editor");
+            }
+        }
+
+        private void CreateAssemblyDefinition(string folderName, string assemblyName, params string[] references)
+        {
+            string folderPath = $"Assets/{folderName}";
+
+            if (!AssetDatabase.IsValidFolder(folderPath))
+            {
+                Debug.LogError("Folder not found: " + folderPath);
+                return;
+            }
+
+            string asmdefPath = Path.Combine(folderPath, $"{assemblyName}.asmdef");
+
+            if (File.Exists(asmdefPath))
+            {
+                return;
+            }
+
+            string referencesJson = "";
+            for (int i = 0; i < references.Length; i++)
+            {
+                referencesJson += $"\"{references[i]}\"";
+                if (i < references.Length - 1)
+                {
+                    referencesJson += ", ";
+                }
+            }
+
+            string asmdefContent = $@"
+        {{
+            ""name"": ""{assemblyName}"",
+            ""references"": [
+                {referencesJson}
+            ],
+            ""includePlatforms"": [],
+            ""excludePlatforms"": [],
+            ""allowUnsafeCode"": false,
+            ""overrideReferences"": false,
+            ""precompiledReferences"": [],
+            ""autoReferenced"": true,
+            ""defineConstraints"": [],
+            ""versionDefines"": [],
+            ""noEngineReferences"": false
+        }}";
+
+            File.WriteAllText(asmdefPath, asmdefContent);
+            AssetDatabase.Refresh();
+        }
+    }
+}
+#endif
